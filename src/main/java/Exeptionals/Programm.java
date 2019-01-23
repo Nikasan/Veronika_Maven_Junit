2f87840444c28cdf0dfab7ec3e5bc9c6eb98bee2
Nikasan <vvlobkova@gmail.com>
2019-01-23 14:42:42 +0300

    add exeptions

--- src/main/java/Exeptionals/Programm.java
+++ src/main/java/Exeptionals/Programm.java
@@ -1,24 +1,48 @@
 package Exeptionals;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Programm {
     public static void main(String[] args) {
-        int [] a = {1,2,3,4,5};
-        try{
+        int[] a = {1, 2, 3, 4, 5};
+        try {
             System.out.println(a[6]);
-        }catch (IndexOutOfBoundsException e){
+        } catch (IndexOutOfBoundsException e) {
             e.printStackTrace();
-        }finally {
-            System.out.println(a);
-            List <Integer> myList = new ArrayList<>();
-
-            myList.add(1);
-
-            public Integer myMethod (int i) throws IndexOutOfBoundsException
-            Integer result = myList.get(0);
-            return result;
+        } finally {
+            System.out.println(a.toString());
 
         }
+        FullExceptionalExample example = new FullExceptionalExample();
+
+    }
 }
+
+
+
+ class FullExceptionalExample {
+            public String input() throws IOException{
+                BufferedReader reader =  new BufferedReader( new InputStreamReader(System.in));
+                String s=null;
+            try{
+                s = reader.readLine();
+            }catch (IOException e){
+                System.out.println(e.getMessage());
+            }finally {
+                try{
+                    reader.close();
+                }catch (IOException e){
+                    System.out.println(e.getMessage());
+                }
+            }
+            if (s.equals("")){
+                throw new IOException("String can not be empty");
+            } return s;
+            }
+        }
+
+
+
+
